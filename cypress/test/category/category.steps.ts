c505891bf3d106578c8c4e3e620616ae83e3f0d8
MahmoudAmin321 <mahmmoud.amin@gmail.com>
2025-06-07 18:34:22 +0300

    category tests v5

--- cypress/test/category/category.steps.ts
+++ cypress/test/category/category.steps.ts
@@ -2,6 +2,8 @@ import { Then, When } from "@badeball/cypress-cucumber-preprocessor";
 import addCategoryPage from "../../pages/admin/categoryForm/addCategory.pom";
 import categoriesPage from "../../pages/admin/categories.pom";
 import { apis } from "../../support/consts";
+import homePage from "../../pages/home.pom";
+import { Base } from "../../pages/_common/base.pom";
 
 Then("option {string} exists", function (bddExactOptionName: string) {
   addCategoryPage.dropdowns
@@ -24,3 +26,26 @@ When("You search for category {string}", function (bddCategoryName: string) {
   // wait
   cy.wait(`@${apis.SearchIncategories.interceptorName}`);
 });
+
+
+Then("You find category {string} in parent categories", function (bddCategoryName: string) {
+  // Find the checkbox with the specific text
+cy.contains('label', bddCategoryName)
+  .children("[data-test^='category']").eq(0).then(($categoryElement) => {
+    // Get the data-test value of this category
+    const categoryId = $categoryElement.attr('data-test');
+    
+    // Verify this category exists in the parent categories list
+    Base.parentCategories()
+      .then(($parentCategories) => {
+        // Convert to array of data-test attributes
+        const parentIds = $parentCategories.map((i, el) => el.getAttribute('data-test')).get();
+        
+        // Assert our category exists in parent categories
+        expect(parentIds).to.include(categoryId);
+      });
+  });
+  
+  
+});
+
